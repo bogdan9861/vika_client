880a60b01198874d037167c4cad8fa1db029354c
vebdev12 <ggkoudi@gmail.com>
2025-11-30 22:59:23 +0300

    change url

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://vika-server.onrender.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
